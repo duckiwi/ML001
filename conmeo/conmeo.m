6c95c567f26b25ed3d228854da56348926ecc27e
PHAM Viet Duc <duc@kiwi-universe.com>
2017-12-13 16:28:30 +0700

    add conmeo.m this is the main function that runs all. all inputs for models will be initialized here.

---
+++ conmeo/conmeo.m
@@ -0,0 +1,37 @@
+%% sample program for machine learning demo
+% the program will use data from coursera assignments
+% the program includes:
+% 
+
+%       import data, extract information
+%       plot data for visualization
+%       linear regression with regulization
+%       logistic regression with regulization
+%       neural network with regulization, along with debug tools
+%       plot result for visualization
+%       cross validation and testing
+%       analyze the role of regulization
+%       analyze the size of training set by prediction accuracy/error and plot training curve
+
+clear; close all; clc
+
+file_name = '';
+data_set = import_data(file_name);
+plot_data(data_set);
+
+%% linear regression
+
+
+
+%% logistic regression
+
+
+
+
+
+%% neural network
+% set up architecture
+
+
+
+
