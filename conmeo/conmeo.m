6ea3bcab8e0f95e0da9b890c2c59fa88a97eaabb
PHAM Viet Duc <duc@kiwi-universe.com>
2017-12-14 17:34:34 +0700

    working on preprocessing data, in progress

--- conmeo/conmeo.m
+++ conmeo/conmeo.m
@@ -5,9 +5,10 @@
 
 %       import data, extract information
 %       plot data for visualization
+%       preprocessing data, with feature scaling
 %       linear regression with regulization
 %       logistic regression with regulization
-%       neural network with regulization, along with debug tools
+%       neural network with regulization, parameter randomization along with debug tools
 %       plot result for visualization
 %       cross validation and testing
 %       analyze the role of regulization
@@ -15,17 +16,38 @@
 
 clear; close all; clc
 
-file_name = '';
-data_set = import_data(file_name);
-plot_data(data_set);
+%% ======================= Part 01: Import data and visualization =======================
+% Input
+file_name = 'ex4data1.mat';
+% Load data
+fprintf('Loading and Visualizing Data ...\n')
+load(file_name);
+m = size(X, 1);
+% Randomly select 100 data points to display
+sel = randperm(size(X, 1));
+sel = sel(1:100);
+displayData(X(sel, :));
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+delete sel
 
-%% linear regression
+%% ======================= Part 02: Extract data =======================
+data_set = readData;
+
+
+
+%% ======================= Part TODO: Linear regression =======================
+% Input
+iteration = 1000;
+
+linRegResult = genLinReg(data_set);
+
 
 
 
 %% logistic regression
 
-
+logRegResult = genLogReg(data_set);
 
 
 
---
+++ conmeo/readData.m
@@ -0,0 +1,16 @@
+%% a script that regroups all information about the input into a cell
+% this code may be changed subject to input format
+% however the format of output (which includes information for training
+% algorithm) stays the same
+
+% TODO describe the output format here
+
+data_set = {};
+
+data_set.X = X;
+data_set.Y = Y;
+data_set.n_examples = m;
+
+out1 = data_set;
+
+
