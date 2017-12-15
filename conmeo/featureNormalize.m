3e5b4c52c22aec71cfa00719caa9e110656db66a
PHAM Viet Duc <duc@kiwi-universe.com>
2017-12-15 17:58:18 +0700

    change nnCostFunction to nnCostFunc finished featureNormalize

--- conmeo/featureNormalize.m
+++ conmeo/featureNormalize.m
@@ -1,33 +1,14 @@
 function [X_norm, mu, sigma] = featureNormalize(X)
-%% TODO change this !!!!
+%%  Normalizes the features in X 
+%   Returns a normalized version of X where the mean value of each feature
+%   is 0 and the standard deviation is 1. 
 
-%FEATURENORMALIZE Normalizes the features in X 
-%   FEATURENORMALIZE(X) returns a normalized version of X where
-%   the mean value of each feature is 0 and the standard deviation
-%   is 1. This is often a good preprocessing step to do when
-%   working with learning algorithms.
-
-% You need to set these values correctly
+%% Initialize input values
 X_norm = X;
 mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: First, for each feature dimension, compute the mean
-%               of the feature and subtract it from the dataset,
-%               storing the mean value in mu. Next, compute the 
-%               standard deviation of each feature and divide
-%               each feature by it's standard deviation, storing
-%               the standard deviation in sigma. 
-%
-%               Note that X is a matrix where each column is a 
-%               feature and each row is an example. You need 
-%               to perform the normalization separately for 
-%               each feature. 
-%
-% Hint: You might find the 'mean' and 'std' functions useful.
-%       
-
+%% Calculate normalized feature
 mu = mean(X);
 for cnt = 1:size(X,2)
     X_norm(:,cnt) = X_norm(:,cnt) - mu(cnt);
@@ -36,7 +17,5 @@ sigma = std(X);
 for cnt = 1:size(X,2)
     X_norm(:,cnt) = X_norm(:,cnt)/sigma(cnt);
 end
-
-% ============================================================
 
 end
